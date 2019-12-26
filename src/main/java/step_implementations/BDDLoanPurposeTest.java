f73d66bcca945f15b86b0142ec0d2bd885517ccc
silver3456 <avseliverov@gmail.com>
2019-12-25 17:27:58 -0800

    Convert steps into gherkin part II

--- src/main/java/step_implementations/BDDLoanPurposeTest.java
+++ src/main/java/step_implementations/BDDLoanPurposeTest.java
@@ -1,25 +1,27 @@
 package step_implementations;
 
-
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BDDLoanPurposeTest {
     protected WebDriver driver;
-    private static int WAIT_TIME = 3000;
+    private static int WAIT_TIME = 5000;
     private static final String COMMON_RADIOBUTTON_PATTERN = "//span[text() = '%s']";
     private static final String ANNUAL_INCOME_INPUT = "//input[@name='employment.incomeYearly']";
     private static final String FIRST_NAME_FIELD = "//input[@placeholder='First name']";
     private static final String LAST_NAME_FIELD = "//input[@placeholder='Last name']";
     private static final String DOB_FIELD = "//input[@placeholder = 'Date of birth (MM/DD/YYYY)']";
     private static final String ADDRESS_FIELD = "//input[@placeholder='Address']";
-    private static final String PATTERN_ADDRESS = "//*[@class = 'geosuggest__item__matched-text']";
+    private static final String PATTERN_ADDRESS = "b.geosuggest__item__matched-text";
     private static final String CONTINUE_BUTTON = "//button[text()= 'Continue']";
     private static final String HOUSING_PAYMENT_FIELD = "//input[@placeholder='Monthly housing payment']";
     private static final String SSN_FIELD = "//input[@placeholder='Social security number']";
@@ -122,41 +124,94 @@ public class BDDLoanPurposeTest {
     @And("^the user enters Address$")
     public void the_user_enters_Address() throws InterruptedException {
         System.out.println("the user enters Address");
-        String streetPartialName = "Cabril";
+        String streetPartialName = "2715 Cabrillo Street, San Francisco";
         driver.findElement(By.xpath(ADDRESS_FIELD)).sendKeys(streetPartialName);
-        driver.findElement(By.xpath(ZIP)).sendKeys("94121");
-        driver.findElement(By.xpath((String.format(PATTERN_ADDRESS, streetPartialName)))).click();
+        Thread.sleep(WAIT_TIME);
+        driver.findElement(By.cssSelector(PATTERN_ADDRESS)).click();
+        Thread.sleep(WAIT_TIME);
         driver.findElement(By.xpath(CONTINUE_BUTTON)).click();
+        Thread.sleep(WAIT_TIME);
     }
 
     //    And the user chooses Housing Status
     @And("^the user chooses Housing Status$")
-    public void the_user_chooses_Housing_Status() {
+    public void the_user_chooses_Housing_Status() throws InterruptedException {
         System.out.println("the user chooses Housing Status");
         String housingStatus = "Rent";
         driver.findElement(By.xpath(String.format(COMMON_RADIOBUTTON_PATTERN, housingStatus))).click();
+        Thread.sleep(WAIT_TIME);
     }
 
     //    And the user enters Monthly Housing Payment
     @And("^the user enters Monthly Housing Payment$")
-    public void the_user_enters_Monthly_Housing_Payment() {
+    public void the_user_enters_Monthly_Housing_Payment() throws InterruptedException {
         System.out.println("the user enters Monthly Housing Payment");
         String housePayment = "1000";
         driver.findElement(By.xpath(HOUSING_PAYMENT_FIELD)).sendKeys(housePayment);
         driver.findElement(By.xpath(CONTINUE_BUTTON)).click();
+        Thread.sleep(WAIT_TIME);
 
     }
-//    And the user chooses Citizenship Status
-//    And the user enters SSN
-//    And the user enters Phone Number
-//    And the user enters Email
-//    And  the user enters Password
+
+    //    And the user chooses Citizenship Status
+    @And("^the user chooses Citizenship Status$")
+    public void the_user_chooses_Citizenship_Status() throws InterruptedException {
+        System.out.println("the user chooses Citizenship Status");
+        String citizenshipStatus = "Yes";
+        driver.findElement(By.xpath(String.format(COMMON_RADIOBUTTON_PATTERN, citizenshipStatus))).click();
+        Thread.sleep(WAIT_TIME);
+    }
+
+    //    And the user enters SSN
+    @And("^the user enters SSN$")
+    public void the_user_enters_SSN() throws InterruptedException {
+        System.out.println("the user enters SSN");
+        String ssn = "134234565";
+        driver.findElement(By.xpath(SSN_FIELD)).sendKeys(ssn);
+        driver.findElement(By.xpath(CONTINUE_BUTTON)).click();
+        Thread.sleep(WAIT_TIME);
+    }
+
+    //    And the user enters Phone Number
+    @And("^the user enters Phone Number$")
+    public void the_user_enters_Phone_Number() throws InterruptedException {
+        System.out.println("the user enters Phone Number");
+        String phoneNum = "1223234565";
+        driver.findElement(By.xpath(PHONE_NUMBER_FIELD)).sendKeys(phoneNum);
+        driver.findElement(By.xpath(CONTINUE_BUTTON)).click();
+        Thread.sleep(WAIT_TIME);
+    }
+
+    //    And the user enters Email
+    @And("^the user enters Email$")
+    public void the_user_enters_Email() throws InterruptedException {
+        System.out.println("the user enters Email");
+        String email = "dfd@kp.org";
+        driver.findElement(By.xpath(EMAIL_FIELD)).sendKeys(email);
+        driver.findElement(By.xpath(CONTINUE_BUTTON)).click();
+        Thread.sleep(WAIT_TIME);
+    }
+
+    //    And  the user enters Password
+    @And("^the user enters Password$")
+    public void the_user_enters_Password() throws InterruptedException {
+        System.out.println("the user enters Password");
+        String password = "dfdfads";
+        driver.findElement(By.xpath(PASSWORD_FIELD)).sendKeys(password);
+        Thread.sleep(WAIT_TIME);
+    }
 
     // Then the submit button becomes clickable
     @Then("^the submit button becomes clickable$")
     public void the_submit_button_becomes_clickable() {
         System.out.println("the submit button becomes clickable");
+        WebElement button = driver.findElement(By.xpath(SUBMIT_BUTTON));
+        Assert.assertTrue(button.isDisplayed());
+    }
 
-
+    @AfterClass
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
     }
 }
